6aa62251eee8be8bdcbffc4f730757962d06cab2
nathantotten <nathan.james.totten@gmail.com>
2025-06-08 22:35:44 +0100

    Backend initial setup

---
+++ backend/learning-tracker/src/main/java/com/caffeinated_dev/learning_tracker/LearningTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.caffeinated_dev.learning_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTrackerApplication.class, args);
+	}
+
+}
